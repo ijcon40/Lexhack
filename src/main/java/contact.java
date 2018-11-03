25c9051cf67396cc8434eeed85a5c2dc501d495d
michecode <matt.busin@protonmail.com>
2018-11-03 16:28:12 -0400

    adding some contact functionality

---
+++ src/main/java/contact.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+public class contact {
+
+    public contact(String name, String localIP, String publicIP, String nickname) {
+        String name;
+        String localIP;
+        String publicIP;
+        String nickname;
+    }
+
+    public static contact[] loadContacts() {
+
+    }
+
+    public static contact newContact() {
+        Scanner input = new Scanner(System.in);
+        String name,localIP,publicIP,nickname;
+        System.out.print("enter name : ");
+        name = input.next();
+        System.out.print("\n + enter nickname : ");
+        nickname = input.next();
+        System.out.print("\n + enter local IP : ");
+        localIP = input.next();
+        System.out.print("\n + enter public IP : ");
+        publicIP = input.next();
+        contact one = new contact(name,localIP,publicIP,nickname);
+        return one;
+    }
+}
